9c6338a7815270f2430ddd8f3b81379a7d719053
Sanyam <sanyambawa96@gmail.com>
2024-11-04 03:39:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
+
+
+// import * as Sentry from '@sentry/react';
+
+
+function App() {
+  
+  return (
+    <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
